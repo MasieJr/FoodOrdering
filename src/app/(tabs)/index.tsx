57a573c0499d8c07a51d44ce4b250c51b60533d2
Masie Junior Seremu <seremu3@gmail.com>
2024-06-22 22:01:41 +0200

    Did a bunch of stuff broke my code. am giving up. will try again tomorrow

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,19 +1,5 @@
-import products from "@assets/data/products"
-import { FlatList } from "react-native";
+import { Redirect } from "expo-router";
 
-import ProductListItem from '@components/productListItem';
-// import { View} from '@/components/Themed';
-
-export default function MEnuScreen() {
-  return (
-    <FlatList 
-      data={products}
-      renderItem={({ item }) => <ProductListItem product={ item }/> }
-      numColumns={2}
-      contentContainerStyle={{gap: 10, padding: 10}}
-      columnWrapperStyle={{gap: 10}} 
-    />
-  )
+export default function TabIndex() {
+    return <Redirect href={'/menu/'} />
 }
-
-
